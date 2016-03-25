afb9b72ba114acee8fb39f7b3d3dadb539fe4c87
Chang Xiangzhong <changxiangzhong@gmail.com>
2016-03-25 15:13:52 +0100

    Read array parameters

--- hello-world/hello.c
+++ hello-world/hello.c
@@ -3,24 +3,44 @@
 #include <linux/module.h>  /* Needed by all modules */
 #include <linux/kernel.h>  /* Needed for KERN_ALERT */
 #include <linux/init.h>
+#include <linux/slab.h>
 
 #define DRIVER_AUTHOR "Chang Xiangzhong"
 #define DRIVER_DESC "A very simple kernel driver"
 
 static unsigned int param1 = 32;
-static char param2[64] = "default param2";
-// static uint param3[10] = {0, 420};
+static char param2[64];
+static unsigned int param3[10];
+static unsigned int param3_len;
 
 module_param(param1, uint, S_IRUGO);
 MODULE_PARM_DESC(param1, "This is the 1st param.");
 
 module_param_string(param2, param2, 64, S_IRUGO);
 MODULE_PARM_DESC(param2, "This is the 2nd param.");
-// module_param(param3, "1-9i", "0444");
+
+module_param_array(param3, uint, &param3_len, S_IRUGO);
+MODULE_PARM_DESC(param3, "This is the 3rd param, which is an array.");
 
 static int hello_2_init(void)
 {
+    int off = 0, i = 0;
+    size_t pt_buf_len = 512;
+//     char print_buf[512];
+    char* print_buf = (char*) kmalloc(pt_buf_len, GFP_KERNEL);
+
+    if (!print_buf) {
+        pr_alert("Failed to allocated memory\n");
+        return 1;
+    }
+
+    for (i = 0; i < param3_len; i++) {
+        off += snprintf(print_buf + off, pt_buf_len - off, "%d, ", param3[i]);
+    }
     pr_alert("Hello, world!\nparam1 = %d, param2 = %s\n", param1, param2);
+    pr_alert("param3 = %s\n", print_buf);
+
+    kfree(print_buf);
     return 0;
 }
 
