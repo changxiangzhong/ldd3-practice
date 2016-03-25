49abeb6088650bb659cd8115d57efcf07c82d8a0
Chang Xiangzhong <changxiangzhong@gmail.com>
2016-03-25 13:08:40 +0100

    Add more functions

--- hello-world/hello.c
+++ hello-world/hello.c
@@ -1,23 +1,40 @@
-#define MODULE
 #define LINUX
-#define __KERNEL__
 
 #include <linux/module.h>  /* Needed by all modules */
 #include <linux/kernel.h>  /* Needed for KERN_ALERT */
+#include <linux/init.h>
 
+#define DRIVER_AUTHOR "Chang Xiangzhong"
+#define DRIVER_DESC "A very simple kernel driver"
 
-int init_module(void)
+static unsigned int param1 = 32;
+static char param2[64] = "default param2";
+// static uint param3[10] = {0, 420};
+
+module_param(param1, uint, S_IRUGO);
+MODULE_PARM_DESC(param1, "This is the 1st param.");
+
+module_param_string(param2, param2, 64, S_IRUGO);
+MODULE_PARM_DESC(param2, "This is the 2nd param.");
+// module_param(param3, "1-9i", "0444");
+
+static int hello_2_init(void)
 {
-	printk("<1>Hello world 1.\n");
-	// A non 0 return means init_module failed; module can't be loaded.
-	return 0;
+    pr_alert("Hello, world!\nparam1 = %d, param2 = %s", param1, param2);
+    return 0;
 }
 
 
-void cleanup_module(void)
+static void hello_2_exit(void)
 {
-	printk(KERN_ALERT "Goodbye world 1.\n");
-}  
+    pr_alert("Goodbye, world 2\n");
+}
+
+module_init(hello_2_init);
+
+module_exit(hello_2_exit);
 
 MODULE_LICENSE("MIT");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
 
